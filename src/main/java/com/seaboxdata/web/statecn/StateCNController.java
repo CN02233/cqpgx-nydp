6d765b040011dd4f41c2072adedbf675578252c5
dfjxps2 <dfjxps2@seaboxdata.com>
2018-08-06 10:07:35 +0800

    statecn

---
+++ src/main/java/com/seaboxdata/web/statecn/StateCNController.java
@@ -0,0 +1,70 @@
+package com.seaboxdata.web.statecn;
+
+import com.seaboxdata.core.base.BaseController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by cc on 2018/8/6.
+ */
+@Controller
+@RequestMapping("/statecn")
+public class StateCNController extends BaseController {
+    /**
+     * 首页-供需
+     */
+    @RequestMapping
+    public String index(ModelMap map){
+        return "page/statecn/index";
+    }
+    /**
+     * 首页-供需(天然气)
+     */
+    @RequestMapping
+    public String gasIndex(ModelMap map){
+        return "page/statecn/gasIndex";
+    }
+    /**
+     * 市场
+     */
+    @RequestMapping
+    public String market(ModelMap map){
+        return "page/statecn/market";
+    }
+    /**
+     * 投资
+     */
+    @RequestMapping
+    public String invest(ModelMap map){
+        return "page/statecn/invest";
+    }
+    /**
+     * 主体
+     */
+    @RequestMapping
+    public String main(ModelMap map){
+        return "page/statecn/main";
+    }
+    /**
+     * 贸易
+     */
+    @RequestMapping
+    public String trade(ModelMap map){
+        return "page/statecn/trade";
+    }
+    /**
+     * 安全
+     */
+    @RequestMapping
+    public String security(ModelMap map){
+        return "page/statecn/security";
+    }
+    /**
+     * 舆情
+     */
+    @RequestMapping
+    public String opinions(ModelMap map){
+        return "page/statecn/opinions";
+    }
+}
